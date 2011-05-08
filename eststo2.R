5dc2f99c71ccb76e6e5b277f23e0df41a15fa16b
Jeffrey Borowitz <borowitz@umd.edu>
2011-05-08 15:04:56 -0400

    Removed options for eststo2.R
    I also finished documenting eststo2.R.  It is possible that in the future I
    should add more attributes to a stored model.  I think in particular each
    model should have it's own coefficient and variance functions.

--- eststo2.R
+++ eststo2.R
@@ -1,39 +1,31 @@
 `eststo2` <-
-function(x,est_column=NULL,stats=NULL, model.name=NULL){ # starting function "eststo"
+function(x,stats=NULL){ # starting function "eststo"
     # stats is a list from list(stat1=value,stat2=value)...  In the
     # default, it will go with the behavior that was already here.
+    # x is a results object, which has the following fields defined.
+    # Standard errors can be calculated by returning:
+    #
+    # sqrt(diag(FUN(x)))
+    # For some function FUN.  If this function is not 'vcov', then the
+    # variance covariance matrix will have to be specified manually during
+    # esttab
 
-    # You can optionally name the model that you are storing by putting a
-    # string as model.name.  If you don't put a string, you will get a name
-    # of 'modeln' where n counts up from 1.
-
-    #print(stats)
-#summary <- summary(x)
-#class <- attributes(x)$class[[1]]
-#coeff <- summary$coefficients
-#vars <- attributes(summary$coeff)$dimnames[[1]] #variable.names(x) 
-#vars <- attr(coef(x),'names') # my version of names
-#col_length <- length(coeff)/4  # number of vars
-#stats <- x$stats
-#rname <- c("results","stats")
-#print(rname)
-#nr <- length(rname)
-#nc <- length(cname)
-
-
-#m[["u", "ave"]] <- ave.u
 if(exists("ccl",where=1)){
     old.results <<- ccl # access current results matrix into old.results 
-    if(is.null(model.name)){
         model.name <- paste('model',length(old.results[1,])+1,sep='')
-    }
+    # Name the model 'modeln'
+
     these.results<- matrix(list(),2,1,dimnames =
                            list(c('stats','results'),c(model.name)))
-    # Make a new matrix of results for this model
+    # Make a new matrix of results for this model, called 'these.results'
 
     if(!is.null(stats)) these.results[['stats',model.name]] <- stats
     else these.results[['stats',model.name]] <- list()
+    # If stats are passed to this model, then put them into the stats
+    # object
+
     these.results[['results',model.name]] <- x
+    # Put x into these results
     return(ccl <<-cbind(old.results,these.results))
 
 }  # grabbing existing list
